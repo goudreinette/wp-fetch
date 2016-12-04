d3e5d8a6f499f1e49e08f8b54c27a5e396ffa60f
reinvdwoerd <reinvanderwoerd@me.com>
2016-12-04 13:54:59 +0100

    init

---
+++ wp-fetch.py
@@ -0,0 +1,28 @@
+from ftputil import FTPHost
+from os      import mkdir, chdir, path
+
+excluded = ["wp-content"]
+
+def download(host, user, password, site):
+    ftpstring = 'ftp.' + host
+    siteroot = 'domains/{host}/public_html/'.format(**locals())
+    with FTPHost(ftpstring, user, password) as host:
+        host.chdir(siteroot)
+        getdir(host, site)
+
+def getfile(host, path):
+    print("File:", path)
+    host.download(path, path)
+
+def getdir(host, dir):
+    print("Dir: ", dir)
+    mkdir(dir)
+    for name in host.listdir(dir):
+        full = path.join(dir, name)
+        if not name in excluded:
+            if host.path.isfile(full):
+                getfile(host, full)
+            else:
+                getdir(host, full)
+
+download('hansvanderwoerd.eu', 'pfed180567', 'lw0OO5f323', 'test')
